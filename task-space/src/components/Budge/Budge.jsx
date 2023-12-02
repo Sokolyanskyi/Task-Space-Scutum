a43594618107918c07ef1161782e61ee9ae84d43
Yaroslav Sokolyansky <93289052+Sokolyanskyi@users.noreply.github.com>
2023-12-02 03:29:01 +0200

    fixing some colors

--- task-space/src/components/Budge/Budge.jsx
+++ task-space/src/components/Budge/Budge.jsx
@@ -1,10 +1,11 @@
 import styles from "./Budge.module.css";
 
 export const Budge = ({todos}) => {
-  return (
+  return (<div className={styles.container}>
 	 <div className={styles.budge}>
 	   <span>У Вас</span>
 	   {!todos.length ? "немає завдань" : todos.length === 1 ? "1 завдання" : todos.length > 1 ? `${todos.length} завдань` : null}
+	 </div>
 	 </div>
   )
 }
